4e166dbdd10c6b3246c8a350f521b5b04d9e00f6
Jamaikah <jamaikahabdul20@gmail.com>
2026-09-12 22:42:04 +0800

    picture album assignment

---
+++ scripts/temples.js
@@ -0,0 +1,9 @@
+document.getElementById("lastModified").textContent = document.lastModified;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+})
